98b9d0bb36308c9206abd2fd0f0f68b0a08d8d92
Jes Anub <mjanub@gmail.com>
2014-02-14 08:31:02 +0800

    Clean up code

--- js/progress-circle.js
+++ js/progress-circle.js
@@ -6,56 +6,50 @@
 
 ( function( $ ){
 	var ProgressCircle = function( element, options ){
-		// Defaults are below
-		var settings = $.extend( {}, $.fn.progressCircle.defaults, options );
-		settings.nPercent > 100 || settings.nPercent < 0 ? settings.nPercent = 0 : settings.nPercent;
-		console.log(settings.nPercent);
-		var nRadians = ( 360 * settings.nPercent ) / 100;
 
-		// Thickness constant
+		var settings          = $.extend( {}, $.fn.progressCircle.defaults, options );
 		var thicknessConstant = 0.02;
-		if ( settings.thickness > 10 ) {
-			settings.thickness = 10;
-		} else if ( settings.thickness < 1 ) {
-			settings.thickness = 1;
-		} else {
-			settings.thickness = Math.round( settings.thickness );
-		}
-		var border = ( settings.thickness * thicknessConstant ) + 'em';
-		var offset = ( 1 - thicknessConstant * settings.thickness * 2 ) + 'em';
+		var nRadian           = 0;
 
-		var circle = $( element );
-		var progCirc = circle.find( '.prog-circle' );
-		var circleDiv = progCirc.find( '.bar' );
-		var circleSpan = progCirc.children( '.percenttext' );
-		var circleFill = progCirc.find( '.fill' );
+		computePercent();
+		setThickness();
+
+		var border      = ( settings.thickness * thicknessConstant ) + 'em';
+		var offset      = ( 1 - thicknessConstant * settings.thickness * 2 ) + 'em';
+		var circle      = $( element );
+		var progCirc    = circle.find( '.prog-circle' );
+		var circleDiv   = progCirc.find( '.bar' );
+		var circleSpan  = progCirc.children( '.percenttext' );
+		var circleFill  = progCirc.find( '.fill' );
 		var circleSlice = progCirc.find( '.slice' );
 
 		if ( settings.nPercent == 0 ) {
 			circleSlice.hide();
 		} else {
-			// Reset circle clip
-			circleSlice.show();
-			circleSpan.text( '' );
-			circleSlice.removeClass( 'clipauto' )
-			transformCircle( 20, circleDiv );
-			transformCircle( 20, circleFill );
-
-			// Draw percentage circle
+			resetCircle();
 			transformCircle( nRadians, circleDiv );
 		}
-
-		// Set circle size
-		progCirc.css( 'font-size', settings.circleSize + 'px' );
 		setBorderThickness();
+		updatePercentage();
+		setCircleSize();
 
-		// Update percentage text if showPercentText is true
-		settings.showPercentText && circleSpan.text( settings.nPercent + '%' );
+		function computePercent () {
+			settings.nPercent > 100 || settings.nPercent < 0 ? settings.nPercent = 0 : settings.nPercent;
+			nRadians = ( 360 * settings.nPercent ) / 100;
+		}
 
-		// Set clip if greater than 50%
-		if( nRadians > 180 ) {
-		    transformCircle( 180, circleFill );
-		    circleSlice.addClass( ' clipauto ');
+		function setThickness () {
+			if ( settings.thickness > 10 ) {
+				settings.thickness = 10;
+			} else if ( settings.thickness < 1 ) {
+				settings.thickness = 1;
+			} else {
+				settings.thickness = Math.round( settings.thickness );
+			}
+		}
+
+		function setCircleSize ( ) {
+			progCirc.css( 'font-size', settings.circleSize + 'px' );
 		}
 
 		function transformCircle ( nRadians, cDiv ) {
@@ -67,6 +61,10 @@
 	      "-o-transform"      : rotate,
 	      "transform"         : rotate
 	    });
+	    if( nRadians > 180 ) {
+		    transformCircle( 180, circleFill );
+		    circleSlice.addClass( ' clipauto ');
+			}
 		}
 
 		function setBorderThickness () {
@@ -82,19 +80,28 @@
 				'height' : offset
 			})
 		}
+
+		function resetCircle () {
+			circleSlice.show();
+			circleSpan.text( '' );
+			circleSlice.removeClass( 'clipauto' )
+			transformCircle( 20, circleDiv );
+			transformCircle( 20, circleFill );
+			return this;
+		}
+
+		function updatePercentage () {
+			settings.showPercentText && circleSpan.text( settings.nPercent + '%' );
+		}
 	};
 
 	$.fn.progressCircle = function( options ) {
 		return this.each( function( key, value ){
       var element = $( this );
-
-      // Return early if this element already has a plugin instance
       if ( element.data( 'progressCircle' ) ) {
       	var progressCircle = new ProgressCircle( this, options );
       	return element.data( 'progressCircle' );
       }
-
-      // Append components
     	$( this ).append( '<div class="prog-circle">' +
 	                 			'	<div class="percenttext"> </div>' +
 	                 			'	<div class="slice">' +
@@ -103,15 +110,11 @@
 	                 			'	</div>' +
 	                 			'	<div class="after"> </div>' +
 	                 			'</div>');
-
-    	// Pass options to plugin constructor
       var progressCircle = new ProgressCircle( this, options );
-      // Store plugin object in this element's data
       element.data( 'progressCircle', progressCircle );
   	});
 	};
 
-	//Default settings
 	$.fn.progressCircle.defaults = {
 		nPercent        : 50,
 		showPercentText : true,
